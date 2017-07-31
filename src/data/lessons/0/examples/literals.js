82e452eeeadf4f9d94c107cb87e05c13131c23dd
alexbaumgertner <alex.baumgertner@gmail.com>
2017-07-31 23:56:04 +0300

    Lesson 0: start Literals

--- src/data/lessons/0/examples/literals.js
+++ src/data/lessons/0/examples/literals.js
@@ -1,4 +1,18 @@
-/* Литералы — записанные непосредственно в коде значения */
-true;
-1;
-'Some string';
+/**
+ @title Литералы — записанные непосредственно в коде значения
+ */
+
+/* Boolean */
+true
+
+/* Number */
+1
+
+/* String */
+'Some string'
+
+/* Object */
+let author = {
+  name: 'Alex',
+  age: 34
+}
